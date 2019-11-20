ed8224d3a566a79e011766bd501b3ae00d037ed7
Takeshi Iwana <iwatakeshi@gmail.com>
2019-11-19 22:11:30 -0700

    Fix types

--- src/configurator/nuxt/main.ts
+++ src/configurator/nuxt/main.ts
@@ -6,34 +6,34 @@ import plugins from './utils/visitors/plugins'
  * @param {String} config The nuxt configuration file
  */
 // tslint:disable-next-line: new-parens
-export default (config: string) => (new (class {
+export default (config: string) => (new (class NuxtConfigurator {
   constructor(private config = '') { }
   addCSS(css: string) {
     this.config = _css.add(this.config, css)
     return this
   }
 
-  removeCSS(css: string) {
+  removeCSS(css: string): NuxtConfigurator {
     this.config = _css.remove(this.config, css)
     return this
   }
 
-  addBuildModule(module: string, options?: string) {
+  addBuildModule(module: string, options?: string): NuxtConfigurator {
     this.config = buildModules.add(this.config, module, options)
     return this
   }
 
-  removeBuildModule(module: string) {
+  removeBuildModule(module: string): NuxtConfigurator {
     this.config = buildModules.remove(this.config, module)
     return this
   }
 
-  addPlugins(plugin: string, mode: 'client' | 'server' = 'server') {
+  addPlugins(plugin: string, mode: 'client' | 'server' = 'server'): NuxtConfigurator {
     this.config = plugins.add(this.config, plugin, mode)
     return this
   }
 
-  configure() {
+  configure(): string {
     return this.config
   }
 })(config))
